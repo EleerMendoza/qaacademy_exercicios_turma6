b04b9c42dd7b69b31cc508b26b149a0e6faf59ef
Eleer <eleerdario12@gmail.com>
2022-09-24 16:44:31 -0300

    Exercicio 5

--- src/exercicios_easy/Exec_5.java
+++ src/exercicios_easy/Exec_5.java
@@ -6,25 +6,46 @@ public class Exec_5 {
 
 
 
+    public int soma (int num, int num1, int num2) {
+        return num+num1+num2;
+    }
 
+    public int sub (int num, int num1, int num2) {
+        return num-num1-num2;
 
-        public static void main(String[] args) {
-            //  Declarar variaveis do tipo numérico: num1, num2, num3;
-            int num1, num2, num3, soma, subtração, multiplicação, divisão , media;
-            String num1Stg = JOptionPane.showInputDialog("Digite o primeiro número");
-            String num2Stg = JOptionPane.showInputDialog("Digite o segundo número");
-            String num3Stg = JOptionPane.showInputDialog("Digite o terceiro número");
+    }
 
-            num1 = Integer.parseInt(num1Stg);
-            num2 = Integer.parseInt(num2Stg);
-            num3 = Integer.parseInt(num3Stg);
+    public int multi (int num, int num1, int num2) {
+        return num*num1*num2;
 
-            System.out.println("A soma dos três números digitaos é:" +(num1+ num2 +num3));
-            System.out.println("A subtração dos três números digitaos é:" +(num1- num2 -num3));
-            System.out.println("A multiplicação dos três números digitaos é:" +(num1* num2 *num3));
-            System.out.println("A média dos três números digitaos é:" +((num1+ num2 +num3)/3));
+    }
+
+    public int media (int num, int num1, int num2) {
+        return (num + num1 +num2)/ 3;
+
     }
 }
+
+
+
+
+//        public static void main(String[] args) {
+//            //  Declarar variaveis do tipo numérico: num1, num2, num3;
+//            int num1, num2, num3, soma, subtração, multiplicação, divisão , media;
+//            String num1Stg = JOptionPane.showInputDialog("Digite o primeiro número");
+//            String num2Stg = JOptionPane.showInputDialog("Digite o segundo número");
+//            String num3Stg = JOptionPane.showInputDialog("Digite o terceiro número");
+//
+//            num1 = Integer.parseInt(num1Stg);
+//            num2 = Integer.parseInt(num2Stg);
+//            num3 = Integer.parseInt(num3Stg);
+//
+//            System.out.println("A soma dos três números digitaos é:" +(num1+ num2 +num3));
+//            System.out.println("A subtração dos três números digitaos é:" +(num1- num2 -num3));
+//            System.out.println("A multiplicação dos três números digitaos é:" +(num1* num2 *num3));
+////            System.out.println("A média dos três números digitaos é:" +((num1+ num2 +num3)/3));
+//    }
+
 
 //Exercício 5 - Faça um algoritmo para ler três números inteiros, armazenar em
 //        variáveis, e exibir a soma, a subtração, multiplicação e a média dos três números
--- src/main/Execusao.java
+++ src/main/Execusao.java
@@ -2,6 +2,7 @@ package main;
 import exercicios_easy.Exec2;
 import exercicios_easy.Exec_3;
 import exercicios_easy.Exec_4;
+import exercicios_easy.Exec_5;
 
 //import exemplos.MetodosSemMain;
 //
@@ -31,12 +32,22 @@ public class Execusao {
 //        System.out.println(exec3.exibirNome("Valeria ", "Vivas"));
 //    }
 
+//    public static void main(String[] args) {
+//
+//        Exec_4 exec_4 = new Exec_4();
+//
+//        System.out.println(exec_4.exibirValor(4));
+
+
     public static void main(String[] args) {
 
-        Exec_4 exec_4 = new Exec_4();
+        Exec_5 exec_5 = new Exec_5();
+        int num = 4, num1 = 2, num2 = 6;
 
-        System.out.println(exec_4.exibirValor(4));
-
+        System.out.println(exec_5.soma(num,  num1, num2));
+        System.out.println(exec_5.sub(num, num1, num2));
+        System.out.println(exec_5.multi(num, num1, num2 ));
+        System.out.println(exec_5.media(num, num1, num2));
     }
 }
 
